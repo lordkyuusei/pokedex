399cbb7f94faef288d1b43359fd9ea0eab105e69
Kevin POIROT <kevin.poirot@epitech.eu>
2021-12-22 18:51:49 +0100

    formatting

--- src/lib/types/PokemonAbility.ts
+++ src/lib/types/PokemonAbility.ts
@@ -1,41 +1,41 @@
-import type { EntityRef } from "./Pokemon";
+import type { EntityRef } from './Pokemon';
 
 export type PokemonAbility = {
-    id: string;
-    name: string;
-    is_main_series: boolean;
-    generation: EntityRef;
-    names: NameRef[];
-    effect_entries: EffectEntryRef[];
-    effect_changes: EffectChangeRef[];
-    flavor_text_entries: FlavorTextEntryRef[];
-    pokemon: PokemonRef[];
-}
+	id: string;
+	name: string;
+	is_main_series: boolean;
+	generation: EntityRef;
+	names: NameRef[];
+	effect_entries: EffectEntryRef[];
+	effect_changes: EffectChangeRef[];
+	flavor_text_entries: FlavorTextEntryRef[];
+	pokemon: PokemonRef[];
+};
 
 type NameRef = {
-    name: string;
-    language: EntityRef;
-}
+	name: string;
+	language: EntityRef;
+};
 
 type EffectEntryRef = {
-    effect: string;
-    short_effect?: string;
-    language: EntityRef;
-}
+	effect: string;
+	short_effect?: string;
+	language: EntityRef;
+};
 
 type EffectChangeRef = {
-    effect_entries: EffectEntryRef[];
-    version_group: EntityRef;
-}
+	effect_entries: EffectEntryRef[];
+	version_group: EntityRef;
+};
 
 type FlavorTextEntryRef = {
-    flavor_text: string;
-    language: EntityRef;
-    version_group: EntityRef;
-}
+	flavor_text: string;
+	language: EntityRef;
+	version_group: EntityRef;
+};
 
 type PokemonRef = {
-    is_hidden: boolean;
-    slot: number;
-    pokemon: EntityRef;
-}
+	is_hidden: boolean;
+	slot: number;
+	pokemon: EntityRef;
+};
