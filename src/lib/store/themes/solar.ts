eb8d551357a2d9a11b69a799c6cfb6bdce72738b
Kyuu <me@lkyuusei.fr>
2022-10-31 20:49:14 +0100

    fix(solar theme): better color too

--- src/lib/store/themes/solar.ts
+++ src/lib/store/themes/solar.ts
@@ -4,10 +4,10 @@ const solar = {
 		"text": 'hsl(0, 0%, 15%)',
 		"border": 'hsl(0, 0%, 15%)',
 		"background": 'hsl(264, 27%, 89%)',
-		"alt-background": 'hsl(264, 27%, 81%)',
-		"light-background": 'hsl(264, 27%, 76%)',
+		"alt-background": 'hsl(264, 27%, 83%)',
+		"light-background": 'hsl(264, 27%, 79%)',
 		"primary": 'hsl(267, 95%, 16%)',
-		"secondary": 'hsl(267, 95%, 24%)',
+		"secondary": 'hsl(267, 95%, 64%)',
 		"cardglass": 'rgba(220, 220, 220, 0.5)',
 		"shadow": '0px 0px 10px 2px hsl(264, 27%, 65%)',
 	}
