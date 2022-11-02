9e9fe94416a4c65989a3447e9ecbf24ac3134fb9
Kyuu <me@lkyuusei.fr>
2022-11-02 10:31:33 +0100

    fix(error page): add custom behavior for 404

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,31 +1,40 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { dev } from '$app/environment';
-	import PokemonLoader from '$lib/components/shared/Loader.svelte';
+	import Loader from '$lib/components/shared/Loader.svelte';
 </script>
 
 <div class="error-page">
-	<h1>Something happened and I'm not sure what exactly</h1>
-	<small>I know exactly what happened, you just can't see it for obvious reasons.</small>
-
-	{#if dev}
-		<h2>More details for the nerds like me</h2>
-		<pre>{$page.status}: [{$page.error.message}]</pre>
+	{#if $page.status === 404}
+		<h1>404.</h1>
+		<small><i>That's an error.</i></small>
+		<p>The resource you tried to look for doesn't exist.</p>
+		<Loader />
 	{:else}
-		<h2>No details because we're in prod.</h2>
-		<small>and i'm supposed to look very p r o f e s s i o n a l.</small>
-		<p>Some stuff you can try:</p>
-		<ul>
-			<li>
-				<b>If you tried to load a pokemon: </b>make sure its number is correct in the URL bar above
-				(like, something between 1 and idk 900)
-			</li>
-			<li>
-				<b>If the number is the right one: </b>the pokemon might be the problem, and it's very
-				likely due to its evolution tree. i'm on it.
-			</li>
-		</ul>
-		<p>You can try to reload something, or look at this ball. Your choice.</p>
-		<PokemonLoader />
+		<h1>Something happened and I'm not sure what exactly</h1>
+		<small>I know exactly what happened, you just can't see it for obvious reasons.</small>
+
+		{#if dev}
+			<h2>More details for the nerds like me</h2>
+			<pre>{$page.status}: [{$page.error.message}]</pre>
+		{:else}
+			<h2>No details because we're in prod.</h2>
+			<small>and i'm supposed to look very p r o f e s s i o n a l.</small>
+		{/if}
 	{/if}
 </div>
+
+<style>
+	.error-page {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+	}
+
+	.error-page h1 {
+		font-size: xx-large;
+		font-weight: bolder;
+	}
+</style>
