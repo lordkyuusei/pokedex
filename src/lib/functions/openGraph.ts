2099f6c90a791309c40ca897c1de5e139f5640c1
Kyuu <me@lkyuusei.fr>
2023-11-23 10:57:32 +0100

    feat(opengraph): add generation, remove long stats name

--- src/lib/functions/openGraph.ts
+++ src/lib/functions/openGraph.ts
@@ -1,8 +1,17 @@
 import type { Pokemon } from "$lib/types/pokeapi/pokemon";
 import type { PokemonSpecie } from "$lib/types/pokeapi/specie";
 
+const mapKeyToStat: { [x: string]: string } = {
+    hp: 'Hp',
+    attack: 'Atk',
+    defense: 'Def',
+    'special-attack': 'Sp.Atk',
+    'special-defense': 'Sp.Def',
+    speed: 'Spe'
+};
+
 export const computeOpenGraphDescription = ({ pokemon, specie }: { pokemon: Pokemon, specie: PokemonSpecie }) => {
     const types = pokemon.types.map(type => type.type.name.toUpperCase()).join('/');
-    const stats = pokemon.stats.map(stat => `${stat.stat.name.toUpperCase()}: ${stat.base_stat}`).join('/');
-    return `${types} Pokemon\n${stats}`;
+    const stats = pokemon.stats.map(stat => `${mapKeyToStat[stat.stat.name]}: ${stat.base_stat}`).join('/');
+    return `${types} Gen ${specie.generation.name} Pokemon\n${stats}`;
 }
