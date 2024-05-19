631b19dcc8d394870afa1a8d59b9c4f8e1be50e1
Kyuu <me@lkyuusei.fr>
2024-05-19 11:35:56 +0200

    fix(redirect): removes id to avoid double id in search

--- src/routes/(app)/pokemon/[id]/+page.server.ts
+++ src/routes/(app)/pokemon/[id]/+page.server.ts
@@ -2,19 +2,8 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (({ params, route, url }) => {
-    console.log(params, route, url);
-
-    const id = parseInt(params.id);
-    if (!Number.isNaN(id) && !url.href.endsWith('stats')) {
-        throw redirect(307, `${id}/stats`);
-    }
+  const id = parseInt(params.id);
+  if (!Number.isNaN(id) && !url.href.endsWith('stats')) {
+    throw redirect(307, `stats`);
+  }
 }) satisfies PageServerLoad;
-
-/*
-
-{ id: '605' } { id: '/(app)/pokemon/[id]' } URL {
-  href: 'http://localhost:5173/pokemon/605',
-  origin: 'http://localhost:5173',
-
-
-*/
